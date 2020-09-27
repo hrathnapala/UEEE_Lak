24bc96d526c7a59a44add94e138ad3aec2f4e9bd
Heshan <hrathnapala@gmail.com>
2020-09-27 21:44:22 +0530

    location filter part added

--- lib/src/screens/filter/location_filter.dart
+++ lib/src/screens/filter/location_filter.dart
@@ -174,6 +174,7 @@ class _LocationFilterState extends State<LocationFilter> {
                                 .pop("${filteredlocation[index]}");
                           },
                         ),
+                        Divider()
                       ],
                     ),
                   );
