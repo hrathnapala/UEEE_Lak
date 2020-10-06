3331f037dfd9709aa8e4c5bfb4797cf9c6710054
imalK96 <imalkrulez@gmail.com>
2020-10-06 20:56:53 +0530

    Added icons for category

--- lib/src/screens/post-ad/category.dart
+++ lib/src/screens/post-ad/category.dart
@@ -8,32 +8,66 @@ class Category extends StatefulWidget {
 
 class CategoryState extends State<Category> {
   final _category = <String>[
-    'Electronics',
-    'Vehicles',
-    'Property',
-    'Home & Garden',
-    'Fashion',
-    'Sports',
-    'Pets',
-    'Services',
-    'Jobs',
-    'Food',
-    'Education'
+    "Essentials",
+    "Electronics",
+    "Vehicles",
+    "Property",
+    "Jobs in Sri Lanka",
+    "Work Overseas",
+    "Home & Garden",
+    "Fashion & Beauty",
+    "Hobby, Sport & Kids",
+    "Business & Industry",
+    "Services",
+    "Education",
+    "Animals",
+    "Agriculture",
+    "Other",
+  ];
+  List<IconData> images = [
+    Icons.local_grocery_store,
+    Icons.phone_android,
+    Icons.local_taxi,
+    Icons.home,
+    Icons.work,
+    Icons.offline_bolt,
+    Icons.location_city,
+    Icons.watch,
+    Icons.play_arrow,
+    Icons.build,
+    Icons.room_service,
+    Icons.book,
+    Icons.pets,
+    Icons.spa,
+    Icons.devices_other,
   ];
   //Location List
+  int counter = 0;
   Widget _buildList() {
     final Iterable<ListTile> tiles = _category.map((String pair) {
-      return ListTile(
-          title: Text(
-            pair,
-            style: TextStyle(fontSize: 16.0),
-          ),
+      var list = ListTile(
+          title: Row(children: [
+            Icon(
+              images[counter],
+              color: Colors.blueAccent,
+            ),
+            Text(
+              '   ' + _category[counter],
+              style: TextStyle(fontSize: 16.0),
+            )
+          ]),
           onTap: () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => ItemDetails()),
             );
           });
+
+      if (counter >= _category.length) {
+        counter = 0;
+      }
+      counter = counter + 1;
+      return list;
     });
 
     final List<Widget> divided =
@@ -51,7 +85,7 @@ class CategoryState extends State<Category> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Post your Ad'),
+        title: Text('Lak.lk'),
         backgroundColor: Colors.blue[900],
       ),
       body: _buildList(),
