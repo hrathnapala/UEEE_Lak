b438380b9ddae9a8f65a016f5a1ca2ceabfca3e2
Heshan <hrathnapala@gmail.com>
2020-10-07 19:04:34 +0530

    Modified

--- lib/src/screens/search/search.dart
+++ lib/src/screens/search/search.dart
@@ -16,7 +16,47 @@ class _SearchState extends State<Search> {
           ),
           centerTitle: true,
         ),
-        body: SearchCategory());
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 5),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.1,
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                      // fillColor: Color(0xffd9d9d9),
+                      // filled: true,
+                      hintText: "Search for a Category",
+                      hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                      contentPadding:
+                          EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 15.0),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 2.0,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 2.0,
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      )),
+                ),
+              ),
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.65,
+                  child: SearchCategory()),
+            ],
+          ),
+        ));
   }
 }
 
